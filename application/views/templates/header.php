d44082b217cf21b69ea067efb295ef0ab82e37d1
Fernad Corbin <60511629+frndcrbn@users.noreply.github.com>
2023-02-24 00:09:07 +0800

    Add files via upload

---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<html>
+        <head>
+                <title>Online Shopping System</title>
+        </head>
+        <body>
+                <h1><?php echo $title; ?></h1>
+                <em>&copy; 2015</em>
+        </body>
+</html>
