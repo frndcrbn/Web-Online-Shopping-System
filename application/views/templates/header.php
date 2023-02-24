bd7f3837606b98935de60224aa60617ce1c80154
Fernad Corbin <60511629+frndcrbn@users.noreply.github.com>
2023-02-25 03:23:34 +0800

    Delete application directory

--- application/views/templates/header.php
+++
@@ -1,9 +0,0 @@
-<html>
-        <head>
-                <title>Online Shopping System</title>
-        </head>
-        <body>
-                <h1><?php echo $title; ?></h1>
-                <em>&copy; 2015</em>
-        </body>
-</html>
